2ea3f78140009ba8cdcfeef389a3e903404687a9
YUKI Hiroshi <shimoda@clear-code.com>
2017-12-13 11:59:19 +0900

    Deactivate headers for notes by default

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -9,7 +9,7 @@ pref("extensions.redthunderminebird.default_description", true);
 pref("extensions.redthunderminebird.default_description_header", true);
 // https://dxr.mozilla.org/comm-central/rev/ce8dba0d6a298a23d651a9622db5520b48ba90cf/mailnews/mime/src/mimedrft.cpp#957
 pref("extensions.redthunderminebird.default_description_header.headers", "Subject,From,Resent-From,Date,To,Cc,Newsgroups");
-pref("extensions.redthunderminebird.default_notes_header", true);
+pref("extensions.redthunderminebird.default_notes_header", false);
 pref("extensions.redthunderminebird.default_notes_header.headers", "Subject,From,Resent-From,Date,To,Cc,Newsgroups");
 pref("extensions.redthunderminebird.default_upload_attachments", true);
 pref("extensions.redthunderminebird.target_project", "");
