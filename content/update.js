417d8c093a3efcd3d798429d2d0bb35b8db9d551
YUKI Hiroshi <shimoda@clear-code.com>
2019-02-21 11:13:07 +0900

    Add ability to set parent issue for new issue

--- content/update.js
+++ content/update.js
@@ -51,8 +51,9 @@ function onLoad() {
 
 	onTicket(ticket);
 
-	if (ticket.parent_issue_id) {
-		var parentTicket = redmine.tryTicket(ticket.parent_issue_id);
+	if (ticket.parent) {
+		document.getElementById('parent_issue_id').value = ticket.parent.id;
+		var parentTicket = redmine.tryTicket(ticket.parent.id);
 		onParentTicket(parentTicket);
 	}
 
@@ -166,25 +167,6 @@ function onRefer() {
 		document.getElementById('id').value = ticket.id;
 		// ↑でonchageは呼ばれない
 		onTicket(ticket);
-		return true;
-	});
-}
-
-function onParentTicket(ticket) {
-	var idField = document.getElementById('parent_issue_id');
-	var id = idField.value;
-	if (!ticket)
-		ticket = redmine.tryTicket(id);
-	var ticket_title = ticket.id ? utility.formatTicketSubject(ticket) : bundle.getLocalString("message.notfoundissue", id);
-	idField.style.width = (String(id || '000').length + 3) + 'ch';
-
-	document.getElementById('parent_ticket_title').value = ticket_title;
-}
-
-function onReferParent() {
-	window.openDialog("chrome://redthunderminebird/content/refer.xul", "referDialog", "chrome,centerscreen,modal", message, function(ticket) {
-		document.getElementById('parent_issue_id').value = ticket.id;
-		onParentTicket(ticket);
 		return true;
 	});
 }
