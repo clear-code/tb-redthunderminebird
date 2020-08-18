54e230231a7dab1750003ee0d71182447da44c9e
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-08-18 11:23:05 +0900

    Add basic options page

---
+++ webextensions/common/common.js
@@ -0,0 +1,35 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+'use strict';
+
+import Configs from '/extlib/Configs.js';
+import * as Constants from './constants.js';
+
+const OVERRIDE_DEFAULT_CONFIGS = {}; /* Replace this for more customization on an enterprise use. */
+
+export const configs = new Configs({
+  configsVersion: 0,
+  debug: false,
+
+  ...OVERRIDE_DEFAULT_CONFIGS
+}, {
+  localKeys: [
+    'configsVersion',
+    'debug'
+  ]
+});
+
+export function log(message, ...args) {
+  if (!configs || !configs.debug)
+    return;
+
+  const nest   = (new Error()).stack.split('\n').length;
+  let indent = '';
+  for (let i = 0; i < nest; i++) {
+    indent += ' ';
+  }
+  console.log(`redthunderminebird: ${indent}${message}`, ...args);
+}
---
+++ webextensions/options/init.js
@@ -0,0 +1,40 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+'use strict';
+
+import {
+  configs,
+  //sendToHost
+} from '/common/common.js';
+import * as Constants from '/common/constants.js';
+import Options from '/extlib/Options.js';
+import '/extlib/l10n.js';
+
+const options = new Options(configs);
+
+function onConfigChanged(key) {
+  switch (key) {
+    case 'debug':
+      document.documentElement.classList.toggle('debugging', configs.debug);
+      break;
+  }
+}
+configs.$addObserver(onConfigChanged);
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await configs.$loaded;
+
+  for (const container of document.querySelectorAll('section, fieldset, p, div')) {
+    const allFields = container.querySelectorAll('input, textarea, select');
+    const lockedFields = container.querySelectorAll('.locked input, .locked textarea, .locked select, input.locked, textarea.locked, select.locked');
+    container.classList.toggle('locked', allFields.length == lockedFields.length);
+  }
+
+  options.buildUIForAllConfigs(document.querySelector('#debug-configs'));
+  onConfigChanged('debug');
+
+  document.documentElement.classList.add('initialized');
+}, { once: true });
