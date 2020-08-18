fdf1fc4e3065238467c67e1176c88e0fd92ee030
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-08-18 17:16:56 +0900

    Lock option UIs at the last of initialization steps

--- webextensions/options/init.js
+++ webextensions/options/init.js
@@ -39,14 +39,14 @@ window.addEventListener('DOMContentLoaded', async () => {
   accountsSelect.value = configs.account || (accounts.length > 0 ? accounts[0].id : '');
 
 
+  options.buildUIForAllConfigs(document.querySelector('#debug-configs'));
+  onConfigChanged('debug');
+
   for (const container of document.querySelectorAll('section, fieldset, p, div')) {
     const allFields = container.querySelectorAll('input, textarea, select');
     const lockedFields = container.querySelectorAll('.locked input, .locked textarea, .locked select, input.locked, textarea.locked, select.locked');
     container.classList.toggle('locked', allFields.length == lockedFields.length);
   }
-
-  options.buildUIForAllConfigs(document.querySelector('#debug-configs'));
-  onConfigChanged('debug');
 
   document.documentElement.classList.add('initialized');
 }, { once: true });
